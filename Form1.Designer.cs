146f442960bfae727110a36bbefa49a82a8088ca
Angela Nyabuti <nyabutiangella@gmail.com>
2022-03-18 18:52:54 +0300

    Added weather API

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -172,6 +172,7 @@ namespace Weather_app
             this.button1.TabIndex = 14;
             this.button1.Text = "Show";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
 
 namespace Weather_app
 {
@@ -25,6 +28,26 @@ namespace Weather_app
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        string APIKey= "41d82f27b1c5fb6ea164a211413a8964";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getWeather();
+        }
+
+        //function to get weather information
+        void getWeather()
+        {
+             using (WebClient web= new WebClient())
+            {
+                //url for downloading weather information
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q=(0)&appid=(1)", txtcity.Text,APIKey);
+                //download weather into JSON object
+                string json = web.DownloadString(url);
+                Weather.root Info = JsonConvert.DeserializeObject<Weather.root>(json);
+                pictureBox1.ImageLocation = "https://home.openweathermap.org/img/W/"+Info.weather[0].icon +".png";
+                label4.Text = Info.wind.speed.ToString();
+            }
         }
     }
 }
